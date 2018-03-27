e0c15b70060805f3b5c39c6af6c044bdade726af
Administrativo <administrativo@ncm.local>
2018-03-27 16:28:02 -0300

    terceiro commit

---
+++ views/especialidade.php
@@ -0,0 +1,186 @@
+<!--
+  Autor: Gleyver Coutinho Castro
+  Data de atualição: 27/03/2018
+  Descrição: Página HTML especialidade
+-->
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Hospital Hhealth</title>
+    <link rel="stylesheet" href="../css/style_especialidade.css">
+    <link rel="stylesheet" type="text/css" href="../css/style_nav.css">
+    <link rel="stylesheet" type="text/css" href="../css/style_footer.css">
+
+  </head>
+  <body>
+    <div class="main"><!--Div Main que segura todas as divs-->
+     <div class="suporte_menu">
+           <div class="menu"><!--**MENU**-->
+              <?php require_once('nav.php'); ?>
+          </div>
+     </div>
+     <div class="div_suporte_conteudo">
+
+     </div>
+     <div id="content_main">
+       <!-- titulo da página -->
+       <div id="suporte_titulo">
+         <div id="titulo_pagina">
+           <strong>Especialidade</strong>
+         </div>
+       </div>
+       <!-- Faixa branca embaixo do menu -->
+       <div class="faixa_branca">
+
+       </div>
+       <!-- Primeira section da div -->
+       <div id="imagem_content">
+         <div id="imagem_hospital">
+           <img src="../imagens/imagem_hospital.jpg" alt="" id="imagem_hosp">
+         </div>
+         <div id="horario_atendimento">
+           <div id="item_atendimento_titulo">
+             <strong>Horário atendimento desta unidade</strong>
+           </div>
+           <div class="faixa_branca_maior">
+
+           </div>
+           <div class="item_atendimento">
+             <div class="dia_atendimento">
+               Segunda a Sexta-feira
+             </div>
+             <div class="hora_atendimento">
+               00h00
+             </div>
+           </div>
+           <div class="faixa_branca_menor">
+
+           </div>
+           <div class="item_atendimento">
+             <div class="dia_atendimento">
+               Sábado
+             </div>
+             <div class="hora_atendimento">
+               00h00
+             </div>
+           </div>
+           <div class="faixa_branca_menor">
+
+           </div>
+           <div class="item_atendimento">
+             <div class="dia_atendimento">
+               Domingo
+             </div>
+             <div class="hora_atendimento">
+               00h00
+             </div>
+           </div>
+           <div class="faixa_branca_menor">
+
+           </div>
+           <div id="item_atendimento_pronto_socorro">
+             <div class="dia_atendimento">
+               <strong>Pronto socorro</strong>
+             </div>
+             <div class="hora_atendimento">
+               24h00
+             </div>
+           </div>
+         </div>
+       </div>
+
+       <!-- Div que mostra o textos das página -->
+         <div id="texto_div">
+           <div  id="suporte_texto">
+             O vídeo fornece uma maneira poderosa de ajudá-lo a provar seu argumento. Ao clicar em Vídeo Online, você pode colar o código de inserção do vídeo que deseja adicionar. Você também pode digitar uma palavra-chave para pesquisar online o vídeo mais adequado ao seu documento.<br/><br/>
+           Para dar ao documento uma aparência profissional, o Word fornece designs de cabeçalho, rodapé, folha de rosto e caixa de texto que se complementam entre si. Por exemplo, você pode adicionar uma folha de rosto, um cabeçalho e uma barra lateral correspondentes. Clique em Inserir e escolha os elementos desejados nas diferentes galerias.
+           Temas e estilos também ajudam a manter seu documento coordenado. Quando você clica em Design e escolhe um novo tema, as imagens, gráficos e elementos gráficos SmartArt são alterados para corresponder ao novo tema. Quando você aplica estilos, os títulos são alterados para coincidir com o novo tema.<br/><br/>
+           Economize tempo no Word com novos botões que são mostrados no local em que você precisa deles. Para alterar a maneira como uma imagem se ajusta ao seu documento, clique nela e um botão de opções de layout será exibido ao lado. Ao trabalhar em uma tabela, clique no local onde deseja adicionar uma linha ou uma coluna e clique no sinal de adição.
+           A leitura também é mais fácil no novo modo de exibição de Leitura. Você pode recolher partes do documento e colocar o foco no texto desejado. Se for preciso interromper a leitura antes de chegar ao fim dela, o Word lembrará em que ponto você parou - até mesmo em outro dispositivo.<br/><br/>
+           O vídeo fornece uma maneira poderosa de ajudá-lo a provar seu argumento. Ao clicar em Vídeo Online, você pode colar o código de inserção do vídeo que deseja adicionar. Você também pode digitar uma palavra-chave para pesquisar online o vídeo mais adequado ao seu documento.<br/><br/>
+           Para dar ao documento uma aparência profissional, o Word fornece designs de cabeçalho, rodapé, folha de rosto e caixa de texto que se complementam entre si. Por exemplo, você pode adicionar uma folha de rosto, um cabeçalho e uma barra lateral correspondentes. Clique em Inserir e escolha os elementos desejados nas diferentes galerias.
+           Temas e estilos também ajudam a manter seu documento coordenado. Quando você clica em Design e escolhe um novo tema, as imagens, gráficos e elementos gráficos SmartArt são alterados para corresponder ao novo tema. Quando você aplica estilos, os títulos são alterados para coincidir com o novo tema.<br/><br/>
+           O vídeo fornece uma maneira poderosa de ajudá-lo a provar seu argumento. Ao clicar em Vídeo Online, você pode colar o código de inserção do vídeo que deseja adicionar. Você também pode digitar uma palavra-chave para pesquisar online o vídeo mais adequado ao seu documento.<br/><br/>
+
+
+           </div>
+         </div>
+         <div id="content_outras_unidades">
+           <div id="suporte_titulo_outras_unidades">
+             <div id="titulo_pagina_outras_unidades">
+               <strong>Outras Unidades do Hhealth com essas especialidades</strong>
+             </div>
+           </div>
+           <!-- Faixa branca embaixo do menu -->
+           <div class="faixa_branca">
+
+           </div>
+
+           <!--  -->
+           <div id="suporte_outras_unidades">
+             <div class="suporte_imagem_texto_outras_unidades">
+               <div class="imagem_outras_unidades">
+                 <img src="../imagens/hospital_barueri.jpg" alt="" class="img_outras_unidades">
+               </div>
+               <div class="titulo_outras_unidades">
+                 <div class="tituloUnidade">
+                   Unidade jandira
+                 </div>
+                 <div class="descricao_unidade">
+                   nao sei sadsajkd nao sei sadsajkd nao sei sadsajkd nao sei sadsajkd nao sei sadsajkd nao sei sadsajkd nao sei sadsajkd nao sei sadsajkd nao sei sadsajkd nao sei sadsajkd nao sei sadsajkd
+                 </div>
+               </div>
+             </div>
+             <div class="suporte_imagem_texto_outras_unidades">
+               <div class="imagem_outras_unidades">
+                 <img src="../imagens/hospital_itapevi.jpg" alt="" class="img_outras_unidades">
+               </div>
+               <div class="titulo_outras_unidades">
+                 <div class="tituloUnidade">
+                   Unidade barueri
+                 </div>
+                 <div class="descricao_unidade">
+                   nao sei sadsajkd nao sei sadsajkd nao sei sadsajkd nao sei sadsajkd nao sei sadsajkd nao sei sadsajkd nao sei sadsajkd nao sei sadsajkd nao sei sadsajkd nao sei sadsajkd nao sei sadsajkd
+                 </div>
+               </div>
+             </div>
+             <div class="suporte_imagem_texto_outras_unidades">
+               <div class="imagem_outras_unidades">
+                 <img src="../imagens/hospital-jandira.jpg" alt="" class="img_outras_unidades">
+               </div>
+               <div class="titulo_outras_unidades">
+                 <div class="tituloUnidade">
+                   Unidade São Paulo
+                 </div>
+                 <div class="descricao_unidade">
+                   nao sei sadsajkd nao sei sadsajkd nao sei sadsajkd nao sei sadsajkd nao sei sadsajkd nao sei sadsajkd nao sei sadsajkd nao sei sadsajkd nao sei sadsajkd nao sei sadsajkd nao sei sadsajkd
+                 </div>
+               </div>
+             </div>
+             <div class="suporte_imagem_texto_outras_unidades">
+               <div class="imagem_outras_unidades">
+                 <img src="../imagens/hospital_sp.jpg" alt="" class="img_outras_unidades">
+               </div>
+               <div class="titulo_outras_unidades">
+                 <div class="tituloUnidade">
+                   Unidade Itapevi
+                 </div>
+                 <div class="descricao_unidade">
+                   nao sei sadsajkd nao sei sadsajkd nao sei sadsajkd nao sei sadsajkd nao sei sadsajkd nao sei sadsajkd nao sei sadsajkd nao sei sadsajkd nao sei sadsajkd nao sei sadsajkd nao sei sadsajkd
+                 </div>
+               </div>
+             </div>
+           </div>
+         </div>
+       </div>
+
+       <!-- Mapa -->
+       <!-- <div id="mapa_unidade">
+         <img src="../imagens/google-maps.jpg" alt="" id="imagem_unidade">
+       </div> -->
+       <?php include_once('footer.php') ?>
+     </div>
+    </div>
+  </body>
+</html>
