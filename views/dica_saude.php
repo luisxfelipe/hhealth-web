d2256230fdbaa40f000398d1f6d80ca929ab2627
Administrativo <administrativo@ncm.local>
2018-03-27 13:06:36 -0300

    primeiro commit

---
+++ views/dica_saude.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Hospital Hhealth</title>
+    <link rel="stylesheet" type="text/css" href="../css/style_dica_saude.css">
+    <link rel="stylesheet" type="text/css" href="../css/style_nav.css">
+    <link rel="stylesheet" type="text/css" href="../css/style_footer.css">
+  </head>
+  <body>
+    <!-- Segura todo conteudo da página -->
+    <div id="main">
+      <!-- inclui menu da página -->
+      <?php require_once('nav.php'); ?>
+      <div id="content_main">
+        <!-- titulo da página -->
+        <div class="suporte_titulo">
+          <div id="titulo_pagina">
+            <strong>Dica</strong>
+          </div>
+        </div>
+        <!-- Faixa branca embaixo do menu -->
+        <div class="faixa_branca">
+
+        </div>
+        <div id="suporte_imagem">
+          <img src="../imagens/maxresdefault.jpg" alt="" id="img_dicas_saude">
+        </div>
+        <div id="suporte_texto">
+          <div id="texto">
+              A primeira dica foi em relação à alimentação. Para começar a comer bem e dar o primeiro passo, é preciso antes pensar em como está a dieta. Será que você come bem? De 3 em 3 horas?
+
+              Uma solução para quem precisa mudar é optar por alimentos 'portáteis', ou seja, pequenos e mais fáceis de comer e carregar, como nozes, castanhas, amêndoas, uvas, maçãs, cenouras pequenas e até tomates cereja. Além disso, para evitar o jejum prolongado, a dica é colocar um alarme no celular para se lembrar de comer a cada 3 horas.
+              <br/>
+              <br/>
+              Nos supermercados, há ainda as opções de alimentos "on the go", práticos para carregar na bolsa e comer a hora que der, como mostrou a reportagem do Phelipe Siani. O conceito começou com a barra de cereal, mas se expandiu para outros produtos, como salada de frutas, salada que já vem com o molho e o garfo, granola, iogurte, frutas secas e diversas outras opções que podem substituir os alimentos mais calóricos e influenciar no modo de vida.
+              A primeira dica foi em relação à alimentação. Para começar a comer bem e dar o primeiro passo, é preciso antes pensar em como está a dieta. Será que você come bem? De 3 em 3 horas?
+
+              Uma solução para quem precisa mudar é optar por alimentos 'portáteis', ou seja, pequenos e mais fáceis de comer e carregar, como nozes, castanhas, amêndoas, uvas, maçãs, cenouras pequenas e até tomates cereja. Além disso, para evitar o jejum prolongado, a dica é colocar um alarme no celular para se lembrar de comer a cada 3 horas.
+              <br/>
+              <br/>
+              Nos supermercados, há ainda as opções de alimentos "on the go", práticos para carregar na bolsa e comer a hora que der, como mostrou a reportagem do Phelipe Siani. O conceito começou com a barra de cereal, mas se expandiu para outros produtos, como salada de frutas, salada que já vem com o molho e o garfo, granola, iogurte, frutas secas e diversas outras opções que podem substituir os alimentos mais calóricos e influenciar no modo de vida.
+              A primeira dica foi em relação à alimentação. Para começar a comer bem e dar o primeiro passo, é preciso antes pensar em como está a dieta. Será que você come bem? De 3 em 3 horas?
+
+              Uma solução para quem precisa mudar é optar por alimentos 'portáteis', ou seja, pequenos e mais fáceis de comer e carregar, como nozes, castanhas, amêndoas, uvas, maçãs, cenouras pequenas e até tomates cereja. Além disso, para evitar o jejum prolongado, a dica é colocar um alarme no celular para se lembrar de comer a cada 3 horas.
+              <br/>
+              <br/>
+              Nos supermercados, há ainda as opções de alimentos "on the go", práticos para carregar na bolsa e comer a hora que der, como mostrou a reportagem do Phelipe Siani. O conceito começou com a barra de cereal, mas se expandiu para outros produtos, como salada de frutas, salada que já vem com o molho e o garfo, granola, iogurte, frutas secas e diversas outras opções que podem substituir os alimentos mais calóricos e influenciar no modo de vida.
+          </div>
+        </div>
+
+        <div class="suporte_titulo">
+          <div id="subtitulo">
+            <strong>Outras Dicas</strong>
+          </div>
+        </div>
+        <!-- Faixa branca embaixo do menu -->
+        <div class="faixa_branca">
+
+        </div>
+        <div class="suporte_outras_dicas">
+          <div class="suporte_imagem_outras_dicas">
+            <img src="../imagens/5dicas.jpg" alt="" class="img_dicas">
+          </div>
+          <div class="suporte_texto_outras_dicas">
+
+          </div>
+        </div>
+        <div class="suporte_outras_dicas">
+          <div class="suporte_imagem_outras_dicas">
+            <img src="../imagens/5dicas.jpg" alt="" class="img_dicas">
+          </div>
+          <div class="suporte_texto_outras_dicas">
+
+          </div>
+        </div>
+        <div class="suporte_outras_dicas">
+          <div class="suporte_imagem_outras_dicas">
+            <img src="../imagens/5dicas.jpg" alt="" class="img_dicas">
+          </div>
+          <div class="suporte_texto_outras_dicas">
+
+          </div>
+        </div>
+      </div>
+      <?php require_once('footer.php'); ?>
+    </div>
+  </body>
+</html>
---
+++ views/unidade_hhealth.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+  <head>
+    <link rel="stylesheet" type="text/css" href="../css/style_nav.css">
+    <link rel="stylesheet" type="text/css" href="../css/style_footer.css">
+    <link rel="stylesheet" type="text/css" href="../css/style_unidade_hhealth.css">
+    <meta charset="utf-8">
+    <title>Hospital HHealth</title>
+  </head>
+  <body>
+    <div id="main">
+      <?php require_once('nav.php'); ?>
+      <div id="content_main">
+        <!-- titulo da página -->
+        <div id="suporte_titulo">
+          <div id="titulo_pagina">
+            <strong>Unidade</strong>
+          </div>
+        </div>
+        <!-- Faixa branca embaixo do menu -->
+        <div class="faixa_branca">
+
+        </div>
+        <!-- Primeira section da div -->
+        <div id="imagem_content">
+          <div id="imagem_hospital">
+            <img src="../imagens/imagem_hospital.jpg" alt="" id="imagem_hosp">
+          </div>
+          <div id="horario_atendimento">
+            <div id="item_atendimento_titulo">
+              <strong>Horário atendimento desta unidade</strong>
+            </div>
+            <div class="faixa_branca_maior">
+
+            </div>
+            <div class="item_atendimento">
+              <div class="dia_atendimento">
+                Segunda a Sexta-feira
+              </div>
+              <div class="hora_atendimento">
+                00h00
+              </div>
+            </div>
+            <div class="faixa_branca_menor">
+
+            </div>
+            <div class="item_atendimento">
+              <div class="dia_atendimento">
+                Sábado
+              </div>
+              <div class="hora_atendimento">
+                00h00
+              </div>
+            </div>
+            <div class="faixa_branca_menor">
+
+            </div>
+            <div class="item_atendimento">
+              <div class="dia_atendimento">
+                Domingo
+              </div>
+              <div class="hora_atendimento">
+                00h00
+              </div>
+            </div>
+            <div class="faixa_branca_menor">
+
+            </div>
+            <div id="item_atendimento_pronto_socorro">
+              <div class="dia_atendimento">
+                <strong>Pronto socorro</strong>
+              </div>
+              <div class="hora_atendimento">
+                24h00
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Div que contém especialidades e o texto -->
+        <div id="especialidades_texto">
+          <div id="especialidades">
+            <div id="titulo_especialidades">
+              <strong>Especialidades</strong>
+            </div>
+            <div class="faixa_branca_maior">
+
+            </div>
+            <div class="especialidade">
+              Especialidade 1
+            </div>
+            <div class="especialidade">
+              Especialidade 2
+            </div>
+            <div class="especialidade">
+              Especialidade 3
+            </div>
+            <div class="especialidade">
+              Especialidade 4
+            </div>
+            <div class="especialidade">
+              Especialidade 5
+            </div>
+            <div class="especialidade">
+              Especialidade 6
+            </div>
+            <div class="especialidade">
+              Especialidade 7
+            </div>
+            <div class="especialidade">
+              Especialidade 8
+            </div>
+            <div class="especialidade">
+              Especialidade 9
+            </div>
+          </div>
+          <div id="texto_div">
+            <div  id="suporte_texto">
+              O vídeo fornece uma maneira poderosa de ajudá-lo a provar seu argumento. Ao clicar em Vídeo Online, você pode colar o código de inserção do vídeo que deseja adicionar. Você também pode digitar uma palavra-chave para pesquisar online o vídeo mais adequado ao seu documento.<br/><br/>
+            Para dar ao documento uma aparência profissional, o Word fornece designs de cabeçalho, rodapé, folha de rosto e caixa de texto que se complementam entre si. Por exemplo, você pode adicionar uma folha de rosto, um cabeçalho e uma barra lateral correspondentes. Clique em Inserir e escolha os elementos desejados nas diferentes galerias.
+            Temas e estilos também ajudam a manter seu documento coordenado. Quando você clica em Design e escolhe um novo tema, as imagens, gráficos e elementos gráficos SmartArt são alterados para corresponder ao novo tema. Quando você aplica estilos, os títulos são alterados para coincidir com o novo tema.<br/><br/>
+            Economize tempo no Word com novos botões que são mostrados no local em que você precisa deles. Para alterar a maneira como uma imagem se ajusta ao seu documento, clique nela e um botão de opções de layout será exibido ao lado. Ao trabalhar em uma tabela, clique no local onde deseja adicionar uma linha ou uma coluna e clique no sinal de adição.
+            A leitura também é mais fácil no novo modo de exibição de Leitura. Você pode recolher partes do documento e colocar o foco no texto desejado. Se for preciso interromper a leitura antes de chegar ao fim dela, o Word lembrará em que ponto você parou - até mesmo em outro dispositivo.<br/><br/>
+            O vídeo fornece uma maneira poderosa de ajudá-lo a provar seu argumento. Ao clicar em Vídeo Online, você pode colar o código de inserção do vídeo que deseja adicionar. Você também pode digitar uma palavra-chave para pesquisar online o vídeo mais adequado ao seu documento.<br/><br/>
+            Para dar ao documento uma aparência profissional, o Word fornece designs de cabeçalho, rodapé, folha de rosto e caixa de texto que se complementam entre si. Por exemplo, você pode adicionar uma folha de rosto, um cabeçalho e uma barra lateral correspondentes. Clique em Inserir e escolha os elementos desejados nas diferentes galerias.
+            Temas e estilos também ajudam a manter seu documento coordenado. Quando você clica em Design e escolhe um novo tema, as imagens, gráficos e elementos gráficos SmartArt são alterados para corresponder ao novo tema. Quando você aplica estilos, os títulos são alterados para coincidir com o novo tema.<br/><br/>
+            O vídeo fornece uma maneira poderosa de ajudá-lo a provar seu argumento. Ao clicar em Vídeo Online, você pode colar o código de inserção do vídeo que deseja adicionar. Você também pode digitar uma palavra-chave para pesquisar online o vídeo mais adequado ao seu documento.<br/><br/>
+
+
+            </div>
+          </div>
+        </div>
+
+        <!-- Mapa -->
+        <div id="mapa_unidade">
+          <img src="../imagens/google-maps.jpg" alt="" id="imagem_unidade">
+        </div>
+
+        <!-- Div localização -->
+        <div id="localizacao_div">
+          <div id="suporte_localização">
+            <div class="texto_localização">
+              Rua dos cáracois , 9532 Itapevi, Saõ Paulo
+            </div>
+            <div class="faixa_branca_footer">
+
+            </div>
+            <div class="texto_localização">
+              Bairro: Jd. Briquet  Cep: 06958 - 986
+            </div>
+            <div class="faixa_branca_footer">
+
+            </div>
+            <div id="suporte_telefone_email">
+              <div class="icon_contato">
+                <img src="../imagens/icons/email.png" alt="" class="icons_contatos">
+              </div>
+              <div id="telefone">
+                (11) 99999-4179
+              </div>
+              <div class="icon_contato">
+                <img src="../imagens/icons/phone_black.png" alt="" class="icons_contatos">
+              </div>
+              <div id="email">
+                hhealth@hospital.com
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <?php include_once('footer.php'); ?>
+    </div>
+  </body>
+</html>
