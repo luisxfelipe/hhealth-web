931bc8077bc2ec85aa34fd82a4fb82821877d13c
Administrativo <administrativo@ncm.local>
2018-03-27 17:04:08 -0300

    quarto commit

--- views/include_area_paciente/area_paciente_agendamento.php
+++ views/include_area_paciente/area_paciente_agendamento.php
@@ -3,12 +3,12 @@
   <head>
     <meta charset="utf-8">
     <title>Agendamento</title>
-    <link rel="stylesheet" href="../css/area_paciente/style_nav.css">
-    <link rel="stylesheet" href="../css/area_paciente/style_agendamento.css">
-    <link rel="stylesheet" href="../css/area_paciente/style_footer.css">
+    <link rel="stylesheet" href="../../css/area_paciente/style_nav.css">
+    <link rel="stylesheet" href="../../css/area_paciente/style_agendamento.css">
+    <link rel="stylesheet" href="../../css/area_paciente/style_footer.css">
   </head>
   <body>
-    <?php include_once('include_area_paciente/nav_paciente.php'); ?>
+    <?php include_once('nav_paciente.php'); ?>
     <div id="content">
       <div id="suporte_titulo">
         <div id="titulo_pagina">
@@ -95,5 +95,6 @@
 
       </div>
     </div>
+    <?php include_once('footer_paciente.php') ?>
   </body>
 </html>
---
+++ views/include_area_paciente/cadastro_paciente.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Hospital Hhealth</title>
+    <link rel="stylesheet" href="../../css/area_paciente/style_nav.css">
+    <link rel="stylesheet" href="../../css/area_paciente/style_cadastro_paciente.css">
+    <link rel="stylesheet" href="../../css/area_paciente/style_footer.css">
+  </head>
+  <body>
+    <div id="main">
+      <?php include_once('nav_paciente.php'); ?>
+      <div id="content">
+        <div id="suporte_titulo">
+          <div id="titulo_pagina">
+            <strong>Cadastro de Paciente</strong>
+          </div>
+        </div>
+        <div class="faixa_branca">
+
+        </div>
+    </div>
+  </body>
+</html>
