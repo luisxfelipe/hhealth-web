68c0035d610ebf4a3d654f99afa651c5ca4df7c0
LUIS FELIPE SIMÕES <16254860@CFP127ALUNO.LOCAL>
2018-03-28 11:05:00 -0300

    fix2 bugs login cms

--- CMS/views_cms/login_cms.php
+++ CMS/views_cms/login_cms.php
@@ -11,21 +11,23 @@
 
                   </div>
                   <div class="login">
-                        <div class="txtlogin">
-                              Login:
-                        </div>
-                        <div class="txt_login">
-                              <input type="text" name="txt_login" value="">
-                        </div>
-                        <div class="txtsenha">
-                              Senha:
-                        </div>
-                        <div class="txt_senha">
-                              <input type="password" name="txt_senha" value="">
-                        </div>
-                        <div class="botao_entar">
-                              <input type="submit" name="btn_entrar" value="Entrar">
-                        </div>
+                        <form name="" method="post" action="router.php?controller=usuario&modo=login">
+                              <div class="txtlogin">
+                                    Login:
+                              </div>
+                              <div class="txt_login">
+                                    <input type="text" name="txt_login" value="">
+                              </div>
+                              <div class="txtsenha">
+                                    Senha:
+                              </div>
+                              <div class="txt_senha">
+                                    <input type="password" name="txt_senha" value="">
+                              </div>
+                              <div class="botao_entar">
+                                    <input type="submit" name="btn_entrar" value="Entrar">
+                              </div>
+                        </form>
                   </div>
 
       </body>
